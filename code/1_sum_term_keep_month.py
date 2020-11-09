5904f256ed88d96678dc47f1acb87ae94300b8d9
Lisa-Stubert <61182572+Lisa-Stubert@users.noreply.github.com>
2020-11-09 19:03:35 +0100

    add data for july and august

--- code/1_sum_term_keep_month.py
+++ code/1_sum_term_keep_month.py
@@ -4,7 +4,7 @@ keep = {}
 list_of_months = [
     #'2019-02','2019-03','2019-04','2019-05','2019-06',
     #'2019-07','2019-08','2019-09','2019-10','2019-11','2019-12',
-    '2020-01','2020-02','2020-03','2020-04','2020-05', '2020-06','2020-07'
+    '2020-01','2020-02','2020-03','2020-04','2020-05', '2020-06','2020-07',
      '2020-08','2020-09','2020-10']
 
 for tmp_month in list_of_months:
@@ -19,10 +19,10 @@ for tmp_month in list_of_months:
             #id_ = id_[1:-1]
             #term = term[1:-1]
             impressions = int(impressions)
-            #visits = int(visits[1:-1])
+            visits = int(visits)
             #month = month[1:-1]
-            print(month)
-            print(tmp_month)
+           # print(month)
+           # print(tmp_month)
             if month == tmp_month:
                 # loop over every term and add to dictionary if not already in
                 if term not in keep.keys():
@@ -36,7 +36,7 @@ for tmp_month in list_of_months:
         f.close()
         
         # save dictionary to new csv
-        with open('data/all_months/processed_al5/1_sum_term_keep_months.csv', 'a') as newf:
+        with open('data/all_months/processed_al5/1_sum_term_keep_months_2020.csv', 'a') as newf:
             for term in keep.keys():
                 fid, imp, vis, mon = keep[term]
                 # add term to csv only if it has at least 2 impressions
